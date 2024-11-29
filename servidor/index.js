43a27574ff4cb9d12e663376fd04256c144f10d1
Luke <pietraheloise5@gmail.com>
2024-11-29 11:58:43 -0300

    Quase Pronto

--- servidor/index.js
+++ servidor/index.js
@@ -28,12 +28,23 @@ app.post('/alunos', (req, res)=>{
 
     const sql='INSERT INTO alunos (nome, cidade, estado) VALUES (?, ?, ?)';
 
-    db.query(sql [nome, cidade, estado], (err, result)=>{
+    db.query(sql, [nome, cidade, estado], (err, result)=>{
         if (err)
         {
+            console.log(err);
             return res.status(500).json({error:'Erro ao cadastrar aluno!'});
         }
-    res.json(results);
+    res.json(result);
+    });
+});
+
+app.get('/alunos', (req, res)=>{
+    const sql= 'SELECT * FROM alunos';
+    db.query(sql, (err, results)=>{
+        if (err) {
+            return res.status(500).json({error: 'Erro ao consultar alunos'});
+        }
+        res.json(results);
     });
 });
 
